a2241a802785948d2a3dc0b01337a92b2de222fc
Renan Luz <ren4n@live.com>
2020-01-08 11:26:11 -0300

    Configurando o Sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'docker',
+  database: 'gobarber',
+  define: {
+    // armazena a data de criação e atualização de cada registro
+    timestamps: true,
+    // evita que se crie tabelas no formato camelCase
+    underscored: true,
+    underscoredAll: true,
+  },
+};
